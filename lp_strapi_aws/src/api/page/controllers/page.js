cf068ffdb8a397b879c79d25558446c58e4dfda1
Ulisses Oliveira de Oliveira <ulissesrce@gmail.com>
2022-03-29 09:43:34 -0300

    Created front with react

--- lp_strapi_aws/src/api/page/controllers/page.js
+++ lp_strapi_aws/src/api/page/controllers/page.js
@@ -1,9 +1,59 @@
-'use strict';
+const { createCoreController } = require("@strapi/strapi").factories;
 
-/**
- *  page controller
- */
+module.exports = createCoreController("api::page.page", ({ strapi }) => ({
+  async find(ctx) {
+    const { query } = ctx;
 
-const { createCoreController } = require('@strapi/strapi').factories;
+    const entity = await strapi.entityService.findMany("api::page.page", {
+      ...query,
+      populate: {
+        Menu: {
+          populate: {
+            title_menu: true,
+            logo_menu: true,
+            link_text: {
+              populate: {
+                link_title: true,
+                url: true,
+              },
+            },
+          },
+        },
+        sections: {
+          populate: {
+            title: true,
+            description: true,
+            image: true,
+            text_grid: {
+              populate: {
+                title: true,
+                description: true,
+              },
+            },
+            image_grid: {
+              populate: {
+                image: true,
+              },
+            },
+            meta_sections: {
+              populate: {
+                name: true,
+                section_id: true,
+                background: true,
+              },
+            },
+          },
+        },
+        Footer: {
+          populate: {
+            text_footer: true,
+          },
+        },
+      },
+    });
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
 
-module.exports = createCoreController('api::page.page');
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
+
